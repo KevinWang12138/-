4d9217ef1d1df2dccf8a0a3ef812f384ef93bf34
KevinWang12138 <wangqianming12138@gmail.com>
2022-05-27 14:57:22 +0800

    代码优化写在这部分分支中

--- src/main/java/com/reggie/config/RedisConfig.java
+++ src/main/java/com/reggie/config/RedisConfig.java
@@ -15,6 +15,7 @@ public class RedisConfig extends CachingConfigurerSupport {
         RedisTemplate<Object,Object> redisTemplate=new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory);
+        System.out.println(1);
         return redisTemplate;
     }
 }
